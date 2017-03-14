a44940f5adc07fa486ee7752a127fe077176d80a
Eqbal Eki <info@eqbalq.com>
2017-03-14 19:36:36 +0200

    Bug fix with saving the data for HOGClassifier.

--- hog_classifier.py
+++ hog_classifier.py
@@ -93,17 +93,7 @@ class HOGClassifier():
             with open(FILE, 'wb') as pfile:
                 pickle.dump(
                     {   'svc':self.svc,
-                        'X_scaller': self.X_scaller,
-                        'color_space': self.color_space,
-                        'spatial_size': self.spatial_size,
-                        'hist_bins': self.hist_bins,
-                        'orient': self.orient,
-                        'pix_per_cell': self.pix_per_cell,
-                        'cell_per_block': self.cell_per_block,
-                        'hog_channel': self.hog_channel,
-                        'spatial_feat': self.spatial_feat,
-                        'hist_feat': self.hist_feat,
-                        'hog_feat':self.hog_feat
+                        'X_scaler': self.X_scaler
                     },
                     pfile, pickle.HIGHEST_PROTOCOL)
         except Exception as e:
